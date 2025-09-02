eeee2f1108d8ef933ed5b763a0f11d5936ec2455
jasielrod <122546513+jasielrod@users.noreply.github.com>
2025-09-02 01:05:45 -0300

    declaracao de variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,26 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Declarando variáveis 
+    char estado1, estado2;
+    char cod1, cod2[2];
+    char cidade1, cidade2;
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int nturisticos1, nturisticos2;
+
+        printf("Desafio Super Trunfo - Países\n");
+        printf("Cadastro das Cartas \n");
+        printf("Digite uma letra de 'A' a 'H' (representando um dos oito estados)\n");
+        scanf("%s", &estado1);
+        printf("Agora digite uma letra de 'A' a 'H' para a carta 2\n");
+        scanf("%s", &estado2);
+
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+        
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
